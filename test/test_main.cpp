6c3c1d30c22e851772d6adfa851604fa265b72cd
Marcelo Lima <marcelowind@gmail.com>
2018-06-24 15:36:38 +0200

    Added verboseness to tests

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,14 +3,24 @@
 //
 
 #include <gtest/gtest.h>
+#include <mfl/args.hpp>
 
 #include "../src/logger.hpp"
 
 namespace logger {
-  Level verboseLevel = logger::LOG;
+  Level verboseLevel = logger::NONE;
 }
 
 int main(int argc, char * argv[]) {
+
+  auto aVerboseLevel = mfl::args::extractOption(argv, argv + argc, "-v");
+  if (aVerboseLevel) {
+    if (!logger::setVerboseLevel(aVerboseLevel)) {
+      LOG(logger::FATAL, "Invalid verbose level set: {:s}", aVerboseLevel);
+      return -1;
+    }
+  }
+
   try {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
